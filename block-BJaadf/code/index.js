c72541d4812532a00af3dff936bf95a9a198937b
seeratbhatia <b.seerat@gmail.com>
2021-12-02 00:38:01 +0530

    block-3

--- block-BJaadf/code/index.js
+++ block-BJaadf/code/index.js
@@ -66,6 +66,10 @@ let numbersThree = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
 
 // Reverse the values of numbersTwo array
 
+
+completed
+
+
 // Join all fruits with '-', convert to uppercase and log it
 
 // Join all fruits with '&', convert to lowercase and log it
